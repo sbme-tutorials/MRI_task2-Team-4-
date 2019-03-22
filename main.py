39d7340d777cd7157a02242741b428848f13eaa3
saharhamdy2020 <36203865+saharhamdy2020@users.noreply.github.com>
2019-03-22 13:12:25 +0200

    Merge branch 'master' of https://github.com/sbme-tutorials/MRI_task2-Team-4-
    okaay

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from task2 import Ui_Form
 from PyQt5.QtWidgets import QFileDialog, QLabel
 from PyQt5.QtGui import QPixmap , QImage
 import sys 
-#import cv2
+from math import*
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -31,9 +31,24 @@ class ApplicationWindow(QtWidgets.QMainWindow):
           # fileName, _filter = QFileDialog.getOpenFileName(self, "Title", "Default File","(*.py)")
           fileName, _ = QFileDialog.getOpenFileName(self," ", "","All Files (*);;Python Files (*.txt)")
           if fileName:
-             PD = open("small_phantom.txt", "rt")     #Opening the txt file
-             print ('\n Matrix f: \n',PD.read())     #Reading it 
-            imgplot = plt.imshow(PD.read())                #ploting the array feh moshkela hnaa
-             self.ui.show_phantom(imgplot)
-             print('by')
-         
+             All =np.load(filename)
+
+            PD=All[0]
+            T1=All[1]
+            T2=All[2]
+            
+            Phantom = qimage2ndarray.array2qimage(PD)
+            pixmap = QPixmap.fromImage(Phantom)
+            self.ui.show_phantom.setPixmap(pixmap)
+            print('by')
+           
+             
+def main():
+     app = QtWidgets.QApplication(sys.argv)
+     application = ApplicationWindow()
+     application.show()
+     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()     
