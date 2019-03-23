21167a2933c92876b1a6441f5d5c5b337856ef9d
Asmaa Rihan <AsmaaRihan@users.noreply.github.com>
2019-03-23 23:17:31 +0200

    last update in K-sapce

--- K-space.py
+++ K-space.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 """
 Created on Thu Mar 21 08:05:46 2019
 
@@ -6,33 +7,45 @@ Created on Thu Mar 21 08:05:46 2019
 """
 
 import numpy as np
-from math import*
- #theta=FA.text() #theta from text box
-phSize=128
-RF=TR
-theta=90
-delta_t=0
-omega=pi*TR*delta_t
-theta=(theta*pi)/180
-Kspace=np.zeros([128,128])
+import matplotlib.pyplot as plt
+from math import exp,cos,sin,pi
+from Phantom import T1,T2
+from PIL import Image
 
-print (theta)
+
+#theta=FA.text() #theta from text box
+phSize=32
+TR=25000
+TE=100
+theta=90
+theta=(theta*pi)/180
+Signal=np.zeros([32,32,3])
+Kspace=np.zeros([32,32],dtype=np.complex)
 Spin_Vector=np.array([[0],[0],[1]])
-print(Spin_Vector)
-for i in range (1,phSize):
-    RX=np.array([[1,0,0],[0,cos(theta),-sin(theta)],[0,sin(theta),cos(theta)]])
-    Spin_Vector=np.dot(RX,Spin_Vector)
-    RX-Y=np.array([[cos(omega.delta_t),-sin(omega.delta_t),0],[sin(omega.delta_t),cos(omega.delta_t),0],[0,0,1]])
-    Spin_Vector=np.dot(RX-Y,Spin_Vector)
-    Decay=([[math.exp(-delta_t/T2[i]),0,0],[0,math.exp(-delta_t/T2[i]),0],[0,0,math.exp(-delta_t/T2[i])]])
-    Spin_Vector=np.dot(Decay,Spin_Vector)
-    Gx_step= ((i-1)/phSize)*2*pi
-    
-    for j in range (1,phSize):
-      Gy_step=((j-1)/phSize)*2*pi 
-      total_theta=(Gx_step*i+Gy_step*j)
-      Kspace[i,k]+=math.exp(total_theta)*Spin_Vector
-      Spin_Vector=np.array([[0],[0],[1-math.exp(delta_t[i]/T1[i])]])+Spin_Vector 
-    
-      
-    
+#print(Spin_Vector.shape)
+for i in range(32):   #kspacerow 
+        for k in range(phSize):
+            for m in range(phSize):
+                RX=np.array([[1,0,0],[0,cos(theta),-sin(theta)],[0,sin(theta),cos(theta)]])
+                #print(RX)
+                #print(Spin_Vector)
+                Spin_Vector=np.dot(RX,Spin_Vector)  #3*1
+               # print(Spin_Vector)
+                Decay=np.array([[exp(-TE/T1[k,m]),0,0],[0,exp(-TE/T1[k,m]),0],[0,0,1-exp(-TR/T2[k,m])]]) #3*3
+               #print(Decay)
+                Spin_Vector=np.dot(Decay,Spin_Vector)  #3*1
+               # print(Spin_Vector)
+                Signal[k,m]=np.transpose(Spin_Vector) #32*32*3
+                #print(Signal[i,j])
+        for j in range(32):
+                Gy=(i/phSize)*(2*pi/180)
+                Gx= (j/phSize)*(2*pi/180)
+                for k in range(32):
+                    for m in range (32):          
+                        alpha=Gx*m+Gy*k
+                        x=Signal[k,m,0]
+                        y=Signal[k,m,1]
+                        z=abs(x+y)
+                        Signal[k,m]=z*complex(cos(alpha),sin(alpha))
+                        Kspace[i,j]+=Signal[k,m,0]+Signal[k,m,1]
+                
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ This is a temporary script file.
 from PyQt5 import QtWidgets, QtCore
 from task2 import Ui_Form
 from PyQt5.QtWidgets import QFileDialog, QLabel
-from PyQt5.QtGui import QPixmap , QImage
+from PyQt5.QtGui import QPixmap , QImage, QPainter
 import sys 
 from math import*
 import qimage2ndarray  
@@ -46,33 +46,31 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             Kspace=np.zeros([32,32],dtype=np.complex)
             Spin_Vector=np.array([[0],[0],[1]])
             #print(Spin_Vector.shape)
-            for i in range(32):   
-                for j in range(32):   
-                    RX=np.array([[1,0,0],[0,cos(theta),-sin(theta)],[0,sin(theta),cos(theta)]])
-                    #print(RX)
-                    #print(Spin_Vector)
-                    Spin_Vector=np.dot(RX,Spin_Vector)  #3*1
-                   # print(Spin_Vector)
-                    Signal[i,j]=np.transpose(Spin_Vector) #32*32*3
-                   #print(Spin_Vector)
-                    Decay=np.array([[exp(-TE/T1[i,j]),0,0],[0,exp(-TE/T1[i,j]),0],[0,0,1-exp(-TR/T2[i,j])]]) #3*3
-                   #print(Decay)
-                    Spin_Vector=np.dot(Decay,Spin_Vector)  #3*1
-                   # print(Spin_Vector)
-                    Signal[i,j]=np.transpose(Spin_Vector) #32*32*3
-                    #print(Signal[i,j])
-            for i in range(32):  #kspace row
-              for j in range(32):   #kspace col
-                    Gy=(i/phSize)*(2*pi/180)
-                    Gx= (j/phSize)*(2*pi/180)
-                    for k in range(32):
-                        for M in range (32):
-                          alpha=Gx*M+Gy*k
-                          x=Signal[i,j,0]
-                          y=Signal[i,j,1]
-                          z=abs(x+y)
-                          Kspace[i,j]=z*complex(cos(alpha),sin(alpha))
-                          #print(Kspace)*
+            for i in range(32):   #kspacerow 
+                    for k in range(phSize):
+                        for m in range(phSize):
+                            RX=np.array([[1,0,0],[0,cos(theta),-sin(theta)],[0,sin(theta),cos(theta)]])
+                            #print(RX)
+                            #print(Spin_Vector)
+                            Spin_Vector=np.dot(RX,Spin_Vector)  #3*1
+                           # print(Spin_Vector)
+                            Decay=np.array([[exp(-TE/T1[k,m]),0,0],[0,exp(-TE/T1[k,m]),0],[0,0,1-exp(-TR/T2[k,m])]]) #3*3
+                           #print(Decay)
+                            Spin_Vector=np.dot(Decay,Spin_Vector)  #3*1
+                           # print(Spin_Vector)
+                            Signal[k,m]=np.transpose(Spin_Vector) #32*32*3
+                            #print(Signal[i,j])
+                    for j in range(32):
+                            Gy=(i/phSize)*(2*pi/180)
+                            Gx= (j/phSize)*(2*pi/180)
+                            for k in range(32):
+                                for m in range (32):          
+                                    alpha=Gx*m+Gy*k
+                                    x=Signal[k,m,0]
+                                    y=Signal[k,m,1]
+                                    z=abs(x+y)
+                                    Signal[k,m]=z*complex(cos(alpha),sin(alpha))
+                                    Kspace[i,j]+=Signal[k,m,0]+Signal[k,m,1]
 
             Kspace=np.fft.ifft2(Kspace)
             Phantom = qimage2ndarray.array2qimage(Kspace)
