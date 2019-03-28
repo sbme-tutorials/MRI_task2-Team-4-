368abccabbadde54d44f8aae9855a6aeb8e6abc2
Asmaa Rihan <AsmaaRihan@users.noreply.github.com>
2019-03-28 12:50:31 +0200

    last modification

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ Spyder Editor
 This is a temporary script file.
 """
 
-from PyQt5 import QtWidgets, QtCore
-from task2 import Ui_Form
-from PyQt5.QtWidgets import QFileDialog, QLabel
-from PyQt5.QtGui import QPixmap , QImage, QPainter
-import sys 
-from math import*
-import qimage2ndarray  
+
+from phantom_gui import Ui_MainWindow
+import sys
+import math
+from math import exp, cos, sin, pi, sqrt
+import traceback
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from math import exp,cos,sin,pi
+from matplotlib import pyplot as plt
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QFileDialog
+from PyQt5.QtGui import QImage, QColor, QBrush, QPainter, QPen, QDragEnterEvent
+from PyQt5.QtCore import Qt
+from PIL import Image, ImageEnhance
+from imageio import imsave, imread
+import scipy.io as sio
+import io
+from time import sleep
+import pyqtgraph as pg
 
 
 
@@ -23,66 +33,209 @@ class ApplicationWindow(QtWidgets.QMainWindow):
     
     def __init__(self):
         super(ApplicationWindow, self).__init__()
-        self.ui = Ui_Form()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
         self.ui.Browse.clicked.connect(self.Browse_clicked)
+        self.ui.showphantom.setMouseTracking(False)
+        self.ui.comboBox.currentIndexChanged.connect(self.choose)
+        self.ui.comboBox_2.currentIndexChanged.connect(self.choose_2)
+        self.brit = 0
+        self.points = QtGui.QPolygon()  #ae al points deh
+        self.estna = False
+        self.size = "128"
+        self.state = "PD"
+        self.ui.TE.clear()
+        self.ui.TR.clear()
+        self.ui.flipangle.clear()
+        self.ui.TE.editingFinished.connect(self.Kspace)
+        self.ui.TR.editingFinished.connect(self.Kspace)
+        self.ui.flipangle.editingFinished.connect(self.Kspace)
+
       
         
-    def Browse_clicked (self) :
-          filename, _ = QFileDialog.getOpenFileName(self," ", "","All Files (*);;Python Files (*.txt)")
-          if filename:
-            All =np.load(filename)
+    def Browse_clicked(self):
+       self.fileName,_ = QFileDialog.getOpenFileName(self," "," ", "All Files (*) ;; Python Files (*.jpg)")
+       if self.fileName:
+         All =np.load(self.fileName)
+         PD=All[0]
+         self.T1=All[1]
+         self.T2=All[2]
+         
+         imsave("Pd.png", PD)
+         imsave("T1.png", self.T1)
+         imsave("T2.png", self.T2)
+         
+         self.myImage = cv2.imread("Pd.png" , cv2.IMREAD_GRAYSCALE)
+         
+         self.fileName0 = "Pd.png"
+         self.fileName1 = "T1.png"
+         self.fileName2 = "T2.png"
+         
+         self.height, self.width = self.myImage.shape
+         
+         self.pixmap = QtGui.QPixmap(self.fileName0)
+         
+         self.ui.showphantom.setScaledContents(True)
+         self.ui.showphantom.setMouseTracking(False)
+         
+         self.ui.showphantom.mouseMoveEvent = self.changeBrit
+         self.ui.showphantom.mousePressEvent = self.getClick
+         
+         self.ui.comboBox.currentIndexChanged.connect(self.choose)
+         self.ui.comboBox_2.currentIndexChanged.connect(self.choose_2)
+         
+         self.estna = True
+         self.Kspace()
+    
+    def mousePressEvent(self, e):
+        self.points << e.pos()
+        self.update()
 
-            PD=All[0]
-            T1=All[1]
-            T2=All[2]
-            phSize=32
-            TR=25000
-            TE=100
-            theta=90
-            theta=(theta*pi)/180
-            Signal=np.zeros([32,32,3])
-            Kspace=np.zeros([32,32],dtype=np.complex)
-            Spin_Vector=np.array([[0],[0],[1]])
-            #print(Spin_Vector.shape)
-            for i in range(32):   #kspacerow 
+# Get the pixel coordinates
+
+    def getClick(self, event):
+        if event.button() == Qt.LeftButton:
+            self.left = True
+            self.right = False
+        if event.button() == Qt.RightButton:
+            self.right = True
+            self.left = False
+        if event.button() == Qt.LeftButton:
+            x = event.pos().x()
+            y = event.pos().y() 
+            print("x = %d, y = %d" % (x,y))
+            x1 = x * (self.myImage.shape[0]/512)
+            y1 = y * (self.myImage.shape[0]/512)
+            x1 = math.floor(x1)
+            y1 = math.floor(y1)
+            self.ui.showphantom.paint = True
+            self.ui.showphantom.point.append([x,y])
+            t = np.arange (0. , 500. ,1.)
+            Mx = np.exp(-t /self.T2[x1][y1])
+            Mz = 1-np.exp(-t/self.T1[x1][y1])
+            self.ui.t1.plot(t , np.ravel(Mx) )
+            self.ui.t2.plot(t , np.ravel(Mz) )
+        if event.button() == Qt.RightButton:
+            self.ui.showphantom.point = []
+            self.ui.t1.clear()
+            self.ui.t2.clear()
+
+
+    def choose(self):
+        self.state = self.ui.comboBox.currentText()
+        if self.state == 'PD':
+                self.pixmap = QtGui.QPixmap(self.fileName0)
+        if self.state == 'T1':
+                self.pixmap = QtGui.QPixmap(self.fileName1)
+        if self.state == 'T2':
+                self.pixmap = QtGui.QPixmap(self.fileName2)
+
+    def choose_2 (self) :
+        self.size = self.ui.comboBox_2.currentText()
+
+
+
+# To change the brightness
+    def changeBrit(self, event):
+        if self.left:
+           self.brit += 0.01
+           im = Image.open(self.fileName0)
+           enhancer = ImageEnhance.Brightness(im)
+           enhanced_im = enhancer.enhance(self.brit)
+           enhanced_im.save('enhanced_pic.jpg')
+           fileName1 = "enhanced_pic.jpg"
+           self.pixmap = QtGui.QPixmap(fileName1)
+        if self.right:
+           self.brit -= 0.01
+           im = Image.open(self.fileName0)
+           enhancer = ImageEnhance.Brightness(im)
+           enhanced_im = enhancer.enhance(self.brit)
+           enhanced_im.save('enhanced_pic.jpg')
+           fileName1 = "enhanced_pic.jpg"
+           self.pixmap = QtGui.QPixmap(fileName1)
+    
+            
+    def Kspace(self):
+        
+                phSize=self.height
+                print(phSize)
+                TR=int(self.ui.TR.text())
+                print(TR)
+                TE=int(self.ui.TE.text())
+                print(TE)
+                theta=int(self.ui.flipangle.text())               
+                print(theta)
+                theta=(theta*pi)/180
+                Signal=np.zeros((phSize,phSize,3))
+                Signal[0:phSize,0:phSize,0]=np.zeros((phSize,phSize))
+                Signal[0:phSize,0:phSize,1]=np.zeros((phSize,phSize))
+                Signal[0:phSize,0:phSize,2]=np.ones((phSize,phSize)) 
+                Kspace=np.zeros((phSize,phSize),dtype=np.complex)
+                RX=np.array([[1,0,0],[0,cos(theta),sin(theta)],[0,-sin(theta),cos(theta)]])
+                for i in range(phSize):   #kspacerow 
                     for k in range(phSize):
                         for m in range(phSize):
-                            RX=np.array([[1,0,0],[0,cos(theta),-sin(theta)],[0,sin(theta),cos(theta)]])
-                            #print(RX)
-                            #print(Spin_Vector)
-                            Spin_Vector=np.dot(RX,Spin_Vector)  #3*1
-                           # print(Spin_Vector)
-                            Decay=np.array([[exp(-TE/T1[k,m]),0,0],[0,exp(-TE/T1[k,m]),0],[0,0,1-exp(-TR/T2[k,m])]]) #3*3
-                           #print(Decay)
-                            Spin_Vector=np.dot(Decay,Spin_Vector)  #3*1
-                           # print(Spin_Vector)
-                            Signal[k,m]=np.transpose(Spin_Vector) #32*32*3
-                            #print(Signal[i,j])
-                    for j in range(32):
-                            Gy=(i/phSize)*(2*pi/180)
-                            Gx= (j/phSize)*(2*pi/180)
-                            for k in range(32):
-                                for m in range (32):          
-                                    alpha=Gx*m+Gy*k
-                                    x=Signal[k,m,0]
-                                    y=Signal[k,m,1]
-                                    z=abs(x+y)
-                                    Signal[k,m]=z*complex(cos(alpha),sin(alpha))
-                                    Kspace[i,j]+=Signal[k,m,0]+Signal[k,m,1]
+                            Signal[k][m]=np.matmul(RX,Signal[k][m])  #3*1
+                            Decay=np.array([[exp(-TE/self.T2[k][m]),0,0],[0,exp(-TE/self.T2[k][m]),0],[0,0,exp(-TE/self.T1[k][m])]]) #3*3
+                            Signal[k][m]=np.matmul(Decay,Signal[k][m])  #3*1                        
+                    for j in range(phSize):
+                         Gy=(j/phSize)*(2*pi) #multi in cols
+                         Gx= (i/phSize)*(2*pi) # *rows
+                         for k in range(phSize):
+                             for m in range (phSize):          
+                                 alpha=Gx*k+Gy*m
+                                 x=Signal[k][m][0]
+                                 y=Signal[k][m][1]
+                                 z=sqrt(x**2+y**2)
+                                 Kspace[i][j]+=z*complex(cos(alpha),sin(alpha)) #remember to divied on 180 and make GX,Gy degrees
+                    Signal[0:phSize][0:phSize][0]=0
+                    Signal[0:phSize][0:phSize][1]=0
+                    for k in range(phSize):
+                        for m in range (phSize):                    #print(Kspace)
+                            Signal[k][m][2]=(1-exp(-TR/self.T1[k][m]))
+                
+                Phantom=np.fft.fft2(Kspace)
+                Phantom1=abs(Phantom)
+                imsave("phantom.png", Phantom1)
+                Kspace1=abs(Kspace)
+                imsave("Kspace.png", Kspace1)
+                ks = QtGui.QPixmap("Kspace.png")
+                ks=ks.scaled(512,512)
+                pixmap = QtGui.QPixmap("phantom.png")
+                pixmap=pixmap.scaled(512,512)
+                 #self.ui.show_phantom.setScaledContents(True)
+                self.ui.constImage.setPixmap(pixmap)
+              #  self.ui.constImage.setPixmap(pixmap)
+                self.ui.kspace.setPixmap(ks)
 
-            Kspace=np.fft.ifft2(Kspace)
-            Phantom = qimage2ndarray.array2qimage(Kspace)
-            pixmap = QPixmap.fromImage(Phantom)
-            self.ui.show_phantom.setScaledContents(True)
-            self.ui.show_phantom.setPixmap(pixmap)
-            print('by')
-          
-         
-            
-    
-        
+
+    def paintEvent(self, event):
+        if self.estna:
+            pixmap = self.pixmap
+            if self.size == '128':
+                pixmap = pixmap.scaled(128,128)
+                self.ui.showphantom.setPixmap(pixmap)# -*- coding: utf-8 -*-
+            if self.size == '64':
+                pixmap = pixmap.scaled(64,64)
+                self.ui.showphantom.setPixmap(pixmap)# -*- coding: utf-8 -*-
+            if self.size == '32':
+                pixmap = pixmap.scaled(32,32)
+                self.ui.showphantom.setPixmap(pixmap)# -*- coding: utf-8 -*-
+            if self.size == '3':
+                pixmap = pixmap.scaled(3,3)
+                self.ui.showphantom.setPixmap(pixmap)# -*- coding: utf-8 -*-
+
+
+
+
+#
+#    def gettext(self, event):
+#        self.flipangle1 = str(self.ui.flipangle.text())
+#        self.TE = str(self.ui.TE.text())
+#        self.TR = str(self.ui.TR.text())
+ 
+
 def main():
      app = QtWidgets.QApplication(sys.argv)
      application = ApplicationWindow()
