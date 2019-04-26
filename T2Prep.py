b7337d4e6457affe09467487b16225868d1f4a09
saharhamdy2020 <36203865+saharhamdy2020@users.noreply.github.com>
2019-04-26 09:38:20 +0200

    Update T2Prep.py

--- T2Prep.py
+++ T2Prep.py
@@ -9,7 +9,8 @@ def T2Prep(self,Signal,TR=1000,TE=200,theta=90):
                 TR=int(self.ui.TR.text())
 
                 theta=(theta*pi)/180
-                T=3*T2
+                TE=3*T2
+                T=TR-TE
                 Signal=np.zeros((self.height,self.width,3))
                 Signal[0:self.height,0:self.width,0]=np.zeros((self.height,self.width))
                 Signal[0:self.height,0:self.width,1]=np.zeros((self.height,self.width))
@@ -19,14 +20,14 @@ def T2Prep(self,Signal,TR=1000,TE=200,theta=90):
                 for i in range(self.height):                 
                     for k in range(self.height):
                         for m in range(self.width):
-                            E = exp(-T/self.T2[k][m])
-                            Signal[k][m]=np.matmul(RX,Signal[k][m])  #3*1
-                            Decay=np.array([[E,0,0],[0,E,0],[0,0,exp(-T/self.T1[k][m])]]) #3*3
-                            Signal[k][m]=np.matmul(Decay,Signal[k][m])  #3*1 
-                            theta=-1*theta
                             E = exp(-TE/self.T2[k][m])
                             Signal[k][m]=np.matmul(RX,Signal[k][m])  #3*1
                             Decay=np.array([[E,0,0],[0,E,0],[0,0,exp(-TE/self.T1[k][m])]]) #3*3
+                            Signal[k][m]=np.matmul(Decay,Signal[k][m])  #3*1 
+                            theta=-1*theta
+                            E = exp(-T/self.T2[k][m])
+                            Signal[k][m]=np.matmul(RX,Signal[k][m])  #3*1
+                            Decay=np.array([[E,0,0],[0,E,0],[0,0,exp(-T/self.T1[k][m])]]) #3*3
                             Signal[k][m]=np.matmul(Decay,Signal[k][m])  
                 
                 
